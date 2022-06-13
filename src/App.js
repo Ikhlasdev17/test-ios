f7f0d15f272fbcf2174922896ee350d32f21668f
Ikhlasdev17 <iklasaralbaev@gmail.com>
2022-06-13 17:36:30 +0500

    test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { Form, Input } from 'antd';
 
 function App() {
   const [name, setName] = useState("");
   return (
     <div className="App">
       <header className="App-header">
-        <input value={name} onChange={e => setName(e.target.value)} placeholder="Enter your name..." />
+        {/* <input value={name} onChange={e => setName(e.target.value)} placeholder="Enter your name..." /> */}
         <p>Hello {name}{name !== "" ? "!" : ''}</p>
+        <Form>
+          <Form.Item>
+          <Input value={name} onChange={e => setName(e.target.value)} />
+          </Form.Item>
+        </Form>
       </header>
     </div>
   );
